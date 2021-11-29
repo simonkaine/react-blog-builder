3c63af5fa47058ba9c2bd1c316120b3dc4c6e975
simonkaine <simonkaine@gmail.com>
2021-11-29 14:51:25 -0800

    subtitle state render

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -8,7 +8,7 @@ import './Home.css'
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title, setTitle] = useState('')
-  // const [subtitle, setSubtitle] = useState()
+  const [subtitle, setSubtitle] = useState('')
   // const [font, setFont] = useState()
   // const [align, setAlign] = useState()
   // const [text, setText] = useState()
@@ -16,8 +16,8 @@ export default function Home() {
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} />
-      <Editor title={title} setTitle={setTitle} />
+      <Preview title={title} subtitle={subtitle} />
+      <Editor title={title} setTitle={setTitle} subtitle={subtitle} setSubtitle={setSubtitle} />
     </main>
   )
 }
